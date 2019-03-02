a826b3bc40093b16cf289d41dda1367f5922ca87
Andrej Deriabin <aderiabin.98@gmail.com>
2019-03-02 18:48:17 +0300

    Improved many client connection test (it still fails).

--- JTMT/src/main/java/JavaTournament/ManyClientTest.java
+++ JTMT/src/main/java/JavaTournament/ManyClientTest.java
@@ -5,6 +5,8 @@ import bgs99c.client.Client;
 public class ManyClientTest {
     private static volatile boolean enableConnection = false;
     private static volatile int clientId = 0;
+    private static volatile int clientsFailed = 0;
+    private static volatile int clientsSucceeded = 0;
 
     public static void main(String[] args) {
         int CLIENT_COUNT = 100;
@@ -14,6 +16,12 @@ public class ManyClientTest {
             t.start();
         }
         enableConnection = true;
+
+        while (clientsFailed + clientsSucceeded < CLIENT_COUNT)
+            Thread.yield();
+
+        System.out.println("Total " + clientsSucceeded + " finished successfully.");
+        System.out.println("Total " + clientsFailed + " failed.");
     }
 
     private static void processClient() {
@@ -37,9 +45,14 @@ public class ManyClientTest {
             client.login("s242322", "waw657");
             System.out.println("Client " + curClient + " declared battle.");
             client.declareBattle("Player4");
+            clientsSucceeded += 1;
         }
         catch (Exception e) {
-            e.printStackTrace();
+            //e.printStackTrace();
+            System.out.println("Client " + curClient + " FAILED TO CONNECT!");
+            StackTraceElement frame = e.getStackTrace()[e.getStackTrace().length - 2];
+            System.out.println(frame.getFileName() + ":" + frame.getLineNumber());
+            clientsFailed += 1;
         }
 
         System.out.println("Client " + curClient + " finished.");
